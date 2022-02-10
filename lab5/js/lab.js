b767efe5c7fda09f98810864d0807f15a935f6ee
angelinacortes <ancortes@csumb.edu>
2022-02-10 12:57:16 -0800

    added lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Angelina Cortes
+ * Created:   02.10.2022
+ * License:   Public Domain
+ **/
+
+// Define Variables
+make = "Chevy";
+model = "Tahoe";
+color = "Black";
+year = "2004";
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
