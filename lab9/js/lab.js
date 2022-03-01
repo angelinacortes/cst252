1911b9c2b47024c5e0ef403c7068a27150e2e156
angelinacortes <ancortes@csumb.edu>
2022-03-01 13:16:23 -0800

    added lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,32 @@
+/*
+ * Author: Angelina Cortes
+ * Created: 01 March 2022
+ * License: Public Domain
+ */
+
+
+// step 2 of task 2
+var outputEl = document.getElementById("div");
+
+
+// step 4 of task 2: button 1
+var oneEl = document.createElement("button")
+
+oneEl.id = "button-one"
+
+oneEl.innerHTML = "Button 1";
+
+oneEl.style.color = "purple";
+
+document.body.appendChild(oneEl);
+
+// step 4 of task 2: button 2
+var twoEl = document.createElement("button")
+
+twoEl.id = "button-two"
+
+twoEl.innerHTML = "Button 2";
+
+twoEl.style.color = "blue";
+
+document.body.appendChild(twoEl);
