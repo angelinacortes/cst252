32456a9d8ad2c9e1a67fedf7799bc739596afa71
angelinacortes <ancortes@csumb.edu>
2022-03-08 13:23:39 -0800

    added lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,12 @@
+/*
+ * Author: Angelina Cortes
+ * Created: 07 March 2022
+ * License: Public Domain
+ */
+
+$(":button");
+$(":button").css("background-color", "#37bd3d");
+$(":button").html("Press Me");
+$("button").click(function(){
+  		alert("Hello!");
+	});
