0c517f4bc7acdcde0e36beedc2283a735e68c5ba
angelinacortes <ancortes@csumb.edu>
2022-04-19 10:52:56 -0700

    added lab 19

---
+++ lab19/js/lab.js
@@ -0,0 +1,41 @@
+/*
+ * Author: Angelina Cortes
+ * Created: 19 April 2022
+ * License: Public Domain
+ */
+
+
+ // Pick an URL from the numbers API: http://numbersapi.com/
+ //var url = "http://numbersapi.com/random/trivia";
+ var url = "https://cataas.com/api/cats?tags=tag1,tag2&skip=0&limit=10";
+ var domain = "https://cataas.com";
+
+ // When a user clicks the button
+ $("#activate").click(getAjax);
+
+ // use a jQuery AJAX call to fetch output from the numbers API
+ function getAjax() {
+   // Using the core $.ajax() method
+   $.ajax({
+       // The URL for the request
+       url: url,
+       // The data to send (will be converted to a query string)
+       // data: { id: 123},
+       // Whether this is a POST or GET request
+       type: "GET",
+       // The type of data we expect back
+       // dataType : "json",
+   })
+   // If the request succeeds
+   .done(function( data ) {
+       //alert("Success!");
+       console.log(data);
+       fullUrl = domain + data.url;
+       // Insert the output in the output div
+       $("#output").html("<img src=" + fullUrl + ">");
+   })
+   // If the request fails
+   .fail(function( xhr, status, errorThrown ) {
+       console.log(errorThrown + " Status:" + status );
+   });
+ }
